cc80a8318d17c5642ea60ee9214505edf332a242
Paige <paigeberliner@gmail.com>
2023-08-08 16:57:06 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+function wrapAdjective(Adjective = 'Special') {
+    if (Adjective === '*') {
+      return function(adjective) {
+        return `You are *${adjective}*!`;
+      }
+    }
+    else {
+      return function(adjective) {
+        return `You are || a dedicated programmer||!`;
+      }
+    }
+  }
